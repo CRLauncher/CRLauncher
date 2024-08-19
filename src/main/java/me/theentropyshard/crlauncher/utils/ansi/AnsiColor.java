2391e872846acbdbbf937718992f7d5a6a0be025
TheEntropyShard <theentropyshard@gmail.com>
2024-08-19 21:21:21 +0300

    Added colors to AnsiColor

--- src/main/java/me/theentropyshard/crlauncher/utils/ansi/AnsiColor.java
+++ src/main/java/me/theentropyshard/crlauncher/utils/ansi/AnsiColor.java
@@ -18,35 +18,36 @@
 
 package me.theentropyshard.crlauncher.utils.ansi;
 
+import java.awt.*;
 import java.util.HashMap;
 import java.util.Map;
 
 public enum AnsiColor {
-    RESET("\u001B[0m"),
-    BLACK("\u001B[30m"),
-    RED("\u001B[31m"),
-    GREEN("\u001B[32m"),
-    YELLOW("\u001B[33m"),
-    BLUE("\u001B[34m"),
-    PURPLE("\u001B[35m"),
-    CYAN("\u001B[36m"),
-    WHITE("\u001B[37m"),
+    RESET("\u001B[0m", null),
+    BLACK("\u001B[30m", new Color(0, 0, 0)),
+    RED("\u001B[31m", new Color(170, 0, 0)),
+    GREEN("\u001B[32m", new Color(0, 170, 0)),
+    YELLOW("\u001B[33m", new Color(193, 156, 0)),
+    BLUE("\u001B[34m", new Color(0, 0, 170)),
+    MAGENTA("\u001B[35m", new Color(170, 0, 170)),
+    CYAN("\u001B[36m", new Color(0, 170, 170)),
+    WHITE("\u001B[37m", new Color(170, 170, 170)),
 
-    BRIGHT_BLACK("\u001B[90m"),
-    BRIGHT_RED("\u001B[91m"),
-    BRIGHT_GREEN("\u001B[92m"),
-    BRIGHT_YELLOW("\u001B[93m"),
-    BRIGHT_BLUE("\u001B[94m"),
-    BRIGHT_PURPLE("\u001B[95m"),
-    BRIGHT_CYAN("\u001B[96m"),
-    BRIGHT_WHITE("\u001B[97m"),
+    BRIGHT_BLACK("\u001B[90m", new Color(85, 85, 85)),
+    BRIGHT_RED("\u001B[91m", new Color(255, 85, 85)),
+    BRIGHT_GREEN("\u001B[92m", new Color(85, 255, 85)),
+    BRIGHT_YELLOW("\u001B[93m", new Color(255, 255, 85)),
+    BRIGHT_BLUE("\u001B[94m", new Color(85, 85, 255)),
+    BRIGHT_MAGENTA("\u001B[95m", new Color(255, 85, 255)),
+    BRIGHT_CYAN("\u001B[96m", new Color(85, 255, 255)),
+    BRIGHT_WHITE("\u001B[97m", new Color(255, 255, 255)),
 
-    BG_BLACK("\u001B[40m"),
+    /*BG_BLACK("\u001B[40m"),
     BG_RED("\u001B[41m"),
     BG_GREEN("\u001B[42m"),
     BG_YELLOW("\u001B[43m"),
     BG_BLUE("\u001B[44m"),
-    BG_PURPLE("\u001B[45m"),
+    BG_MAGENTA("\u001B[45m"),
     BG_CYAN("\u001B[46m"),
     BG_WHITE("\u001B[47m"),
 
@@ -55,11 +56,11 @@ public enum AnsiColor {
     BRIGHT_BG_GREEN("\u001B[102m"),
     BRIGHT_BG_YELLOW("\u001B[103m"),
     BRIGHT_BG_BLUE("\u001B[104m"),
-    BRIGHT_BG_PURPLE("\u001B[105m"),
+    BRIGHT_BG_MAGENTA("\u001B[105m"),
     BRIGHT_BG_CYAN("\u001B[106m"),
-    BRIGHT_BG_WHITE("\u001B[107m"),
+    BRIGHT_BG_WHITE("\u001B[107m"),*/
 
-    NONE("");
+    NONE("", null);
 
     private static final Map<String, AnsiColor> lookup = new HashMap<>();
 
@@ -70,9 +71,11 @@ public enum AnsiColor {
     }
 
     private final String code;
+    private final Color color;
 
-    AnsiColor(String code) {
+    AnsiColor(String code, Color color) {
         this.code = code;
+        this.color = color;
     }
 
     public static AnsiColor of(String code) {
@@ -87,6 +90,10 @@ public enum AnsiColor {
 
     public String getCode() {
         return this.code;
+    }
+
+    public Color getColor() {
+        return this.color;
     }
 
     public static String stripAnsiCodes(String input) {
