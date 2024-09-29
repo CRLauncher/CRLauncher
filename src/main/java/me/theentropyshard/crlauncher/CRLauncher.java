aab1d574eec44237ffd1cf7b685e15fd18f08dae
StartsMercury <89975834+StartsMercury@users.noreply.github.com>
2024-09-29 21:04:13 +0800

    Add Filipino and Tagalog Translations

--- src/main/java/me/theentropyshard/crlauncher/CRLauncher.java
+++ src/main/java/me/theentropyshard/crlauncher/CRLauncher.java
@@ -131,7 +131,7 @@ public class CRLauncher {
 
         this.languages = new LinkedHashMap<>();
 
-        for (String lang : new String[]{"en_US", "ru_RU"}) {
+        for (String lang : new String[]{"en_US", "fil_PH", "ru_RU", "tl_PH"}) {
             String resourcePath = "/lang/" + lang + ".json";
 
             String json = null;
