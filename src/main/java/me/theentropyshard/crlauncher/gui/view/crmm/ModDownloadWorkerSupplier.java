f7c62816138d4323847ec936739c38195cc1739e
TheEntropyShard <theentropyshard@gmail.com>
2024-10-01 20:04:24 +0300

    Fixed error downloading mod if mods directory does not exist

--- src/main/java/me/theentropyshard/crlauncher/gui/view/crmm/ModDownloadWorkerSupplier.java
+++ src/main/java/me/theentropyshard/crlauncher/gui/view/crmm/ModDownloadWorkerSupplier.java
@@ -115,6 +115,8 @@ public class ModDownloadWorkerSupplier implements WorkerSupplier {
                     return null;
                 }
 
+                FileUtils.createDirectoryIfNotExists(modFolder);
+
                 Path modPath = Files.move(
                     saveAs, modFolder.resolve(saveAs.getFileName()), StandardCopyOption.REPLACE_EXISTING
                 );
