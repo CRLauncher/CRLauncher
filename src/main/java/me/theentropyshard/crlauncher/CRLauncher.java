4f09862dc5aec961423bded6f8cd25666dd307fd
TheEntropyShard <theentropyshard@gmail.com>
2024-10-16 13:56:48 +0300

    Added German by Rektic

--- src/main/java/me/theentropyshard/crlauncher/CRLauncher.java
+++ src/main/java/me/theentropyshard/crlauncher/CRLauncher.java
@@ -135,7 +135,7 @@ public class CRLauncher {
 
         this.languages = new LinkedHashMap<>();
 
-        for (String lang : new String[]{"en_US", "fil_PH", "ru_RU", "tl_PH"}) {
+        for (String lang : new String[]{"de_DE", "en_US", "fil_PH", "ru_RU", "tl_PH"}) {
             String resourcePath = "/lang/" + lang + ".json";
 
             String json = null;
