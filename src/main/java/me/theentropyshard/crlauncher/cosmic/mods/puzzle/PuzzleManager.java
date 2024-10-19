68cb9027d0e1cac368baa080d2630bb03a5f6a31
TheEntropyShard <theentropyshard@gmail.com>
2024-10-19 17:43:50 +0300

    Updated Puzzle dependencies

--- src/main/java/me/theentropyshard/crlauncher/cosmic/mods/puzzle/PuzzleManager.java
+++ src/main/java/me/theentropyshard/crlauncher/cosmic/mods/puzzle/PuzzleManager.java
@@ -27,6 +27,7 @@ import me.theentropyshard.crlauncher.network.download.HttpDownload;
 import me.theentropyshard.crlauncher.network.progress.ProgressListener;
 import me.theentropyshard.crlauncher.utils.FileUtils;
 import me.theentropyshard.crlauncher.utils.ListUtils;
+import me.theentropyshard.crlauncher.utils.SemanticVersion;
 
 import java.io.File;
 import java.io.IOException;
@@ -37,6 +38,16 @@ import java.util.List;
 public class PuzzleManager {
     private static final List<PuzzleDependency> LIBRARIES = List.of(
         new PuzzleDependency("https://repo.spongepowered.org/repository/maven-public/", new MavenArtifact("org.spongepowered", "mixin", "0.8.5")),
+        new PuzzleDependency("https://jitpack.io/", new MavenArtifact("com.github.PuzzleLoader", "access_manipulators", "1.0.1")),
+        new PuzzleDependency("https://repo1.maven.org/maven2/", new MavenArtifact("org.ow2.asm", "asm", "9.6")),
+        new PuzzleDependency("https://repo1.maven.org/maven2/", new MavenArtifact("org.ow2.asm", "asm-tree", "9.6")),
+        new PuzzleDependency("https://repo1.maven.org/maven2/", new MavenArtifact("org.ow2.asm", "asm-util", "9.6")),
+        new PuzzleDependency("https://repo1.maven.org/maven2/", new MavenArtifact("org.ow2.asm", "asm-analysis", "9.6")),
+        new PuzzleDependency("https://repo1.maven.org/maven2/", new MavenArtifact("org.ow2.asm", "asm-commons", "9.6"))
+    );
+
+    private static final List<PuzzleDependency> LIBRARIES_2_0_0 = List.of(
+        new PuzzleDependency("https://maven.fabricmc.net/", new MavenArtifact("net.fabricmc", "sponge-mixin", "0.15.3+mixin.0.8.7")),
         new PuzzleDependency("https://jitpack.io/", new MavenArtifact("com.github.PuzzleLoader", "access_manipulators", "1.0.1")),
         new PuzzleDependency("https://repo1.maven.org/maven2/", new MavenArtifact("org.ow2.asm", "asm", "9.6")),
         new PuzzleDependency("https://repo1.maven.org/maven2/", new MavenArtifact("org.ow2.asm", "asm-tree", "9.6")),
@@ -84,7 +95,7 @@ public class PuzzleManager {
 
         list.add(loaderDownload);
 
-        for (PuzzleDependency dependency : PuzzleManager.LIBRARIES) {
+        for (PuzzleDependency dependency : PuzzleManager.getLibraries(version)) {
             MavenArtifact artifact = dependency.mavenArtifact();
 
             HttpDownload libDownload = new HttpDownload.Builder()
@@ -113,7 +124,7 @@ public class PuzzleManager {
             return false;
         }
 
-        for (PuzzleDependency dependency : PuzzleManager.LIBRARIES) {
+        for (PuzzleDependency dependency : PuzzleManager.getLibraries(version)) {
             MavenArtifact artifact = dependency.mavenArtifact();
 
             if (!Files.exists(this.depsDir.resolve(artifact.jar()))) {
@@ -122,5 +133,16 @@ public class PuzzleManager {
         }
 
         return true;
+    }
+
+    public static List<PuzzleDependency> getLibraries(String version) {
+        SemanticVersion puzzleVersion = SemanticVersion.parse(version);
+        SemanticVersion version2_0_0 = new SemanticVersion(2, 0, 0);
+
+        if (puzzleVersion.isLowerThan(version2_0_0)) {
+            return PuzzleManager.LIBRARIES;
+        } else {
+            return PuzzleManager.LIBRARIES_2_0_0;
+        }
     }
 }
