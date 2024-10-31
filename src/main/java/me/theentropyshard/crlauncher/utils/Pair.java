4557f5bd0a17e55ecfdd5500441e7a11540a037d
TheEntropyShard <theentropyshard@gmail.com>
2024-10-31 22:58:39 +0300

    Added Pair class

---
+++ src/main/java/me/theentropyshard/crlauncher/utils/Pair.java
@@ -0,0 +1,45 @@
+/*
+ * CRLauncher - https://github.com/CRLauncher/CRLauncher
+ * Copyright (C) 2024 CRLauncher
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package me.theentropyshard.crlauncher.utils;
+
+public class Pair<L, R> {
+    private L left;
+    private R right;
+
+    public Pair(L left, R right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    public L getLeft() {
+        return this.left;
+    }
+
+    public void setLeft(L left) {
+        this.left = left;
+    }
+
+    public R getRight() {
+        return this.right;
+    }
+
+    public void setRight(R right) {
+        this.right = right;
+    }
+}
