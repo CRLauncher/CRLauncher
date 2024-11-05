a83a422500b7c66dfd2d2e60db443c4004974e4c
TheEntropyShard <theentropyshard@gmail.com>
2024-11-05 18:11:30 +0300

    Updated to version 0.8.2

--- src/main/java/me/theentropyshard/crlauncher/cosmic/launcher/PatchCosmicLauncher.java
+++ src/main/java/me/theentropyshard/crlauncher/cosmic/launcher/PatchCosmicLauncher.java
@@ -34,9 +34,9 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class PatchCosmicLauncher extends AbstractCosmicLauncher {
-    private static final SemanticVersion CR_LOADER_VERSION = SemanticVersion.parse("0.1.4");
+    private static final SemanticVersion CR_LOADER_VERSION = SemanticVersion.parse("0.1.5");
     private static final String CR_LOADER_JAR = "CRLoader-" + PatchCosmicLauncher.CR_LOADER_VERSION.toVersionString() + ".jar";
-    private static final String CR_LOADER_SHA256 = "7870359e7500e5a52999631528be2c010ab2dfe46d8788e64d42e0ee21440f47";
+    private static final String CR_LOADER_SHA256 = "0086752ef682c5704208e932310e269181dcee2c766bcf20c48642f4860f266e";
 
     private boolean changeSaveLocation;
     private String customWindowTitle;
