6ec7b28404b9958e1248958d2c4edcdad78edc4e
TheEntropyShard <theentropyshard@gmail.com>
2024-12-26 11:55:19 +0300

    Changed border, now padding is a bit more consistent

--- src/main/java/me/theentropyshard/crlauncher/gui/view/crmm/SearchCrmmModsView.java
+++ src/main/java/me/theentropyshard/crlauncher/gui/view/crmm/SearchCrmmModsView.java
@@ -58,7 +58,7 @@ public class SearchCrmmModsView extends JPanel {
 
             }
         };
-        this.modCardsPanel.setBorder(new EmptyBorder(0, 3, 0, 10));
+        this.modCardsPanel.setBorder(new EmptyBorder(0, 0, 0, 10));
         JPanel borderPanel = new JPanel(new BorderLayout());
         borderPanel.add(this.modCardsPanel, BorderLayout.PAGE_START);
 
