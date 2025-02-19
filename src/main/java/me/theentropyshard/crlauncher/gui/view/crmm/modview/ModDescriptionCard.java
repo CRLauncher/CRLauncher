03c1a5ae6d718d564e2185e9655bbeac932308ee
TheEntropyShard <theentropyshard@gmail.com>
2025-02-19 21:50:58 +0300

    Fixed tables for new mod view

--- src/main/java/me/theentropyshard/crlauncher/gui/view/crmm/modview/ModDescriptionCard.java
+++ src/main/java/me/theentropyshard/crlauncher/gui/view/crmm/modview/ModDescriptionCard.java
@@ -21,6 +21,8 @@ package me.theentropyshard.crlauncher.gui.view.crmm.modview;
 import me.theentropyshard.crlauncher.crmm.model.project.Project;
 import me.theentropyshard.crlauncher.gui.BrowseHyperlinkListener;
 import me.theentropyshard.crlauncher.gui.components.Card;
+import org.commonmark.Extension;
+import org.commonmark.ext.gfm.tables.TablesExtension;
 import org.commonmark.node.Node;
 import org.commonmark.parser.Parser;
 import org.commonmark.renderer.html.HtmlRenderer;
@@ -29,14 +31,19 @@ import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import javax.swing.text.html.HTMLEditorKit;
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ModDescriptionCard extends Card {
     public ModDescriptionCard(Project project) {
         this.setLayout(new BorderLayout());
 
-        Parser parser = Parser.builder().build();
+        List<Extension> extensions = new ArrayList<>();
+        extensions.add(TablesExtension.create());
+
+        Parser parser = Parser.builder().extensions(extensions).build();
         Node document = parser.parse(project.getDescription());
-        HtmlRenderer renderer = HtmlRenderer.builder().build();
+        HtmlRenderer renderer = HtmlRenderer.builder().extensions(extensions).build();
         String html = renderer.render(document);
 
         JTextPane changelogPane = new JTextPane();
@@ -45,7 +52,11 @@ public class ModDescriptionCard extends Card {
         changelogPane.setFont(changelogPane.getFont().deriveFont(14.0f));
         changelogPane.setContentType("text/html");
         changelogPane.setEditorKit(new HTMLEditorKit());
-        changelogPane.setText(html);
+        changelogPane.setText(
+            "<html><body><div style='width:800px'>" +
+            html
+            + "</div></body></html>"
+        );
         changelogPane.setEditable(false);
         changelogPane.setBackground(this.getDefaultColor());
 
